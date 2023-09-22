ef6e2e5a957818a9187f7e4d54787ac5bd4c5305
wangd-aa <wangd-aa@glodon.com>
2023-09-22 18:13:31 +0800

    update api

---
+++ api/type/[type].ts
@@ -0,0 +1,15 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { createMockData } from "../../mock";
+import { SegmentNodeType } from "../../model";
+
+export default function handler(
+	request: VercelRequest,
+	response: VercelResponse
+) {
+	const { type } = request.query;
+	// 生成随机数据
+	const { data } = createMockData(type as SegmentNodeType);
+	console.log(data);
+	// 将随机数据作为JSON响应发送
+	return response.json(data);
+}
